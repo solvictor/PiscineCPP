a908faa1fa6f2c395c97f70997161e9266dd1c0d
Victor Egret <victor.egret.pro@gmail.com>
2023-06-23 14:57:22 +0200

    Better tests

--- CPP02/ex03/main.cpp
+++ CPP02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vegret <victor.egret.pro@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/18 17:02:56 by vegret            #+#    #+#             */
-/*   Updated: 2023/06/18 23:45:22 by vegret           ###   ########.fr       */
+/*   Updated: 2023/06/23 14:56:42 by vegret           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,20 @@ int main()
 	Point b(0.5f, 0);
 	Point c(1.5f, 2);
 	
-	Point d(1.2f, 1.42f);
+	Point inside(1.2f, 1.42f);
 
-	if (bsp(a, b, c, d))
+	if (bsp(a, b, c, inside))
 		std::cout << "Le point est dans le triangle" << std::endl;
 	else
 		std::cout << "Le point n'est pas dans le triangle" << std::endl;
+
+	Point outside(-1.0f, 0.6f);
+
+	if (bsp(a, b, c, outside))
+		std::cout << "Le point est dans le triangle" << std::endl;
+	else
+		std::cout << "Le point n'est pas dans le triangle" << std::endl;
+
 
 	return 0;
 }
