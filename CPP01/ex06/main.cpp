5f7148ecbe3704b1232f2faf2c8a6b00f27f7a45
Victor Egret <victor.egret.pro@gmail.com>
2023-07-12 16:29:33 +0200

    Update

--- CPP01/ex06/main.cpp
+++ CPP01/ex06/main.cpp
@@ -6,17 +6,23 @@
 /*   By: vegret <victor.egret.pro@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 19:18:23 by vegret            #+#    #+#             */
-/*   Updated: 2023/06/18 16:30:58 by vegret           ###   ########.fr       */
+/*   Updated: 2023/06/23 18:41:33 by vegret           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include "Harl.hpp"
 
-int main() // TODO Finir
+int main(int argc, char *argv[]) // TODO Finir
 {
-	Harl	harl;
+	if (argc != 2)
+	{
+		std::cout << "Usage: " << argv[0] << " <level>" << std::endl;
+		return 1;
+	}
+
+	Harl harl;
 	
-	harl.complain("mhmmhmhhmhmmhmh");
-	harl.complain("DEBUG");
+	harl.complain(argv[1]);
 	return 0;
 }
