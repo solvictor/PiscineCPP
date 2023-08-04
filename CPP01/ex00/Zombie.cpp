b62bcb0cbce8abb3a9df1d373952607a72df8721
Victor Egret <victor.egret.pro@gmail.com>
2023-08-04 16:56:51 +0200

    Destructor message

--- CPP01/ex00/Zombie.cpp
+++ CPP01/ex00/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: vegret <victor.egret.pro@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 16:00:12 by vegret            #+#    #+#             */
-/*   Updated: 2023/06/15 18:19:42 by vegret           ###   ########.fr       */
+/*   Updated: 2023/08/04 16:55:27 by vegret           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ Zombie::Zombie() : nom("Inconnu") {}
 
 Zombie::Zombie(std::string nom) : nom(nom) {}
 
-Zombie::~Zombie() {}
+Zombie::~Zombie()
+{
+	std::cout << this->nom << " est mort (vraiment cette fois)" << std::endl;
+}
 
 void Zombie::announce(void)
 {
