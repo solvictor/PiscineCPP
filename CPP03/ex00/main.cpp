f04a0496133724fae8199429bccfe5631a16223d
Victor Egret <victor.egret.pro@gmail.com>
2023-08-13 19:36:17 +0200

    More tests

--- CPP03/ex00/main.cpp
+++ CPP03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vegret <victor.egret.pro@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/18 17:02:56 by vegret            #+#    #+#             */
-/*   Updated: 2023/06/19 17:17:19 by vegret           ###   ########.fr       */
+/*   Updated: 2023/08/13 19:21:12 by vegret           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,11 @@ int main()
 {
 	ClapTrap joe("Joe");
 	ClapTrap bill("Bill");
+	ClapTrap cpy(joe);
 
 	bill.attack("Joe");
 	joe.beRepaired(10);
-	joe.takeDamage(10);
+	cpy.takeDamage(10);
 	joe.takeDamage(10);
 	joe.takeDamage(10);
 	joe.attack("Bill");
