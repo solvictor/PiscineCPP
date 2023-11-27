9b6ff912731b8e60a0cd040c2ae3df38ea4486d7
Victor Egret <victor.egret.pro@gmail.com>
2023-11-27 18:32:21 +0100

    Cleanup on sort

--- CPP09/ex02/PmergeMe.hpp
+++ CPP09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: vegret <victor.egret.pro@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 15:03:43 by vegret            #+#    #+#             */
-/*   Updated: 2023/11/24 16:32:57 by vegret           ###   ########.fr       */
+/*   Updated: 2023/11/27 18:30:55 by vegret           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,20 +28,20 @@ class PmergeMe {
 		}
 
 		template <typename T>
-		static void insertion(T& result, size_t element) {
+		static void binary_insert(T& container, size_t element) {
 			int left = 0;
-			int right = result.size() - 1;
+			int right = container.size() - 1;
 			int mid;
 
 			while (left <= right) {
 				mid = left + (right - left) / 2;
-				if (element < result[mid])
+				if (element < container[mid])
 					right = mid - 1;
 				else
 					left = mid + 1;
 			}
 
-			result.insert(result.begin() + left, element);
+			container.insert(container.begin() + left, element);
 		}
 
 	public:
@@ -52,38 +52,24 @@ class PmergeMe {
 
 		template <typename T>
 		static void sort(T& container) {
-			static int jacobsthal[] = {0, 2, 4, 10, 20, 42, 84, 170, 340, 682, 1364, 2730, 5460, 10922, 21844, 43690, 87380, 174762, 349524, 699050, 1398100, 2796202, 5592404, 11184810, 22369620, 44739242, 89478484, 178956970, 357913940, 715827882, 1431655764, 2147483647};
+
+			if (container.size() < 2)
+				return;
 
 			sort_pairs(container);
 
-			if (container.size() < 3)
-				return;
-
-			T smallests, biggests;
-			for (size_t i = 0; i < container.size(); i += 2) {
-				smallests.push_back(container[i]);
-				if (i + 1 < container.size())
-					biggests.push_back(container[i + 1]);
-			}
+			T biggests;
+			for (size_t i = 1; i < container.size(); i += 2)
+				biggests.push_back(container[i]);
 		
-			sort(smallests);
+			sort(biggests);
 
-			for (size_t index = 0; index < biggests.size(); ) {
-				int jacobsthal_index = 0;
-				while (static_cast<int>(index) < jacobsthal[jacobsthal_index])
-					jacobsthal_index++;
+			for (size_t i = 0; i < container.size(); i += 2)
+				binary_insert(biggests, container[i]);
 
-				if (jacobsthal_index) {
-					if (static_cast<int>(biggests.size() - 1) <= jacobsthal[jacobsthal_index])
-						jacobsthal_index = biggests.size() - (index - jacobsthal[jacobsthal_index - 1]);
-					else
-						jacobsthal_index = jacobsthal[jacobsthal_index] + 1 - (index - jacobsthal[jacobsthal_index - 1]);
-				}
-				insertion(smallests, biggests[jacobsthal_index]);
-				biggests.erase(biggests.begin() + jacobsthal_index);
-			}
-			container = smallests;
+			container = biggests;
 		}
+
 };
 
 #endif
